1a090a26e765aa49886d2a218179fa432fddccfb
Leon Feder <39753469+leonfeder@users.noreply.github.com>
2019-04-26 12:52:12 -0400

    Add files via upload

---
+++ google-sheet.js
@@ -0,0 +1,65 @@
+// Variable to hold request
+//
+// Leon notes: There's a bunch of jquery here!
+var request;
+// var message;
+// Bind to the submit event of our form
+$("#draftPlayer").submit(function(event){
+    alert("Hey");
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to the Google Sheet script
+    //
+    request = $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbzgfKcKo7rj_kcrebQZNhETxpnp0D9iPTmPf_16RA/exec",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+        console.log(response);
+        console.log(textStatus);
+        console.log(jqXHR);
+        // message = response + " >>> " + textStatus + " >>> " + jqXHR;
+        alert(textStatus);
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+        alert("It failed!")
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+    // Prevent default posting of form
+    event.preventDefault();
+});
